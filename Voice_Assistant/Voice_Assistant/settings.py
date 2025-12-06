654d4684aed8f35827f1aa847cce623959a21e43
Chitwan rana <kushr4446@gmail.com>
2025-12-06 13:00:49 +0530

    Update Rhubarb

--- Voice_Assistant/Voice_Assistant/settings.py
+++ Voice_Assistant/Voice_Assistant/settings.py
@@ -82,13 +82,7 @@ DATABASES = {
     }
 }
 
-STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
-STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Password validation
@@ -125,8 +119,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+# Media files (User uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
